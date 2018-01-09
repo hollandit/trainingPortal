ef7b7c4c1176b249d06d3698667753ab12bd7ad4
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-01-09 15:10:11 +0300

    setting path

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,24 @@
-const path = require('path');
+/* eslint-disable no-undef */
+const path = require("path");
 
 module.exports = {
-    entry: __dirname + "/public/js/AuthForm.js",
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname + "public/src")
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            },
-            {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
-    }
+	entry: __dirname + "/public/js/AuthForm.js",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname + "/public/client")
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: "babel-loader"
+			},
+			{
+				test: /\.jsx$/,
+				exclude: /node_modules/,
+				loader: "babel-loader"
+			}
+		]
+	}
 };
