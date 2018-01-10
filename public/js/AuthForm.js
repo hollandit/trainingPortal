558f9926219164648c462f8d556c47d8ad756a94
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-01-10 10:44:55 +0300

    added module material-ui

--- public/js/AuthForm.js
+++ public/js/AuthForm.js
@@ -1,7 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TextField from 'material-ui/TextField';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
+
+function TextFields() {
+    return(
+        <div>
+            <form>
+                <Fields text="Введите логин"/>
+                <Fields text="Введите пароль"/>
+            </form>
+        </div>
+    );
+}
+
+class Fields extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <MuiThemeProvider>
+            <div>
+                <TextField
+                    hintText={this.props.text}
+                />
+            </div>
+            </MuiThemeProvider>
+        );
+    }
+}
 
 ReactDOM.render(
-    <div>Hello world!</div>,
+    <TextFields />,
     document.getElementById('content')
 );
