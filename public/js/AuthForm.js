9773814b06ee6859930db5199ffa8fbf55c8f337
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-01-10 15:33:57 +0300

    Edit for form authentification

--- public/js/AuthForm.js
+++ public/js/AuthForm.js
@@ -1,40 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TextField from 'material-ui/TextField';
+import RaiseButtin from 'material-ui/RaisedButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 injectTapEventPlugin();
 
-function TextFields() {
-    return(
-        <div>
-            <form>
-                <Fields text="Введите логин"/>
-                <Fields text="Введите пароль"/>
-            </form>
-        </div>
-    );
-}
+class UserForm extends React.Component{
+    state = {
+        login: "",
+        password: "",
+    };
 
-class Fields extends React.Component{
-    constructor(props){
-        super(props);
+    handleChange = e => {
+        const { name, value } = e.target;
+
+        this.setState({
+            [name]: value,
+        });
     }
     render(){
+        const { login, password } = this.state;
         return(
-            <MuiThemeProvider>
             <div>
-                <TextField
-                    hintText={this.props.text}
-                />
+                <form method='post' action="/">
+                    <MuiThemeProvider>
+                        <TextField
+                            hintText="Введите логин"
+                            name="login"
+                            value={login}
+                            onChange={this.handleChange}
+                        />
+                    </MuiThemeProvider>
+                    <MuiThemeProvider>
+                        <TextField
+                            hintText="Введите пароль"
+                            type="password"
+                            name="password"
+                            value={password}
+                            onChange={this.handleChange}
+                        />
+                    </MuiThemeProvider>
+                    <MuiThemeProvider>
+                        <RaiseButtin label="Войти" fullWidth={true} className="authForm-button"> <input type="submit" className="authForm-input" /> </RaiseButtin>
+                    </MuiThemeProvider>
+                </form>
             </div>
-            </MuiThemeProvider>
         );
     }
 }
 
 ReactDOM.render(
-    <TextFields />,
+    <UserForm />,
     document.getElementById('content')
 );
