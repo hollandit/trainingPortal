4f5d908a949f99243465d97eaf2e23900dfe313d
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-01-11 11:54:23 +0300

    added passport and keys for session

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ const Koa = require('koa'),
     server = require('koa-static'),
     logger = require('koa-logger'),
     session = require('koa-session'),
+    passport = require('./bin/passport'),
     router = require('./router'),
     app = new Koa();
 
+app.keys = ['SecreT Keys'];
 app.use(bodyParse());
 app.use(session({}, app));
 app.use(logger());
 app.use(server(__dirname + '/public'));
 app.use(views(__dirname + '/public', {extension: 'pug'}));
-
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(router.routes());
 
