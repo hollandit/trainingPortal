c0641111bbd7f0bcbf75b198d2a2d86ecacb5e62
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-01-22 10:30:12 +0300

    added BrowserRouter module react-router-dom

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Switch } from 'react-router';
+import { Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import Auth from './component/AuthForm';
 import Helmet from './aplication';
 
 ReactDOM.render(
-    <Helmet title='Авторизация'/>,
-    <Router>
-        <Switch>
-            <Route path='/' component={Auth}/>
-        </Switch>
-    </Router>,
+    <div>
+        <Helmet title='Авторизация'/>,
+        <BrowserRouter>
+            <Switch>
+                <Route path='/' component={Auth}/>
+            </Switch>
+        </BrowserRouter>
+    </div>,
     document.getElementById('content')
 );
