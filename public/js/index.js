fc6d40d52c2127267b0b1419628a5efe77a4236a
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-01-23 16:50:41 +0300

    added redux

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import  { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { Route, Switch } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import Auth from './component/AuthForm';
+import Home from './component/Home';
 import Helmet from './aplication';
 
+function user(state = {}, action){
+    if(action.type === 'ADD_USER'){
+        return [
+            ...state,
+            action.user
+        ];
+    }
+    return state;
+}
+
+const store = createStore(user, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 ReactDOM.render(
-    <div>
-        <Helmet title='Авторизация'/>,
-        <BrowserRouter>
-            <Switch>
-                <Route path='/' component={Auth}/>
-            </Switch>
-        </BrowserRouter>
-    </div>,
+        <div>
+        <Helmet title='Авторизация'/>
+        <Provider store={store}>
+            <BrowserRouter>
+                    <Switch>
+                        <Route exact path='/' component={Auth}/>
+                        <Route path='/index' component={Home}/>
+                    </Switch>
+            </BrowserRouter>
+        </Provider>
+        </div>,
     document.getElementById('content')
 );
