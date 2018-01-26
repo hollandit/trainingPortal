3ab441f84cdd617bf5b53f3ad79cf029448a23d0
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-01-26 19:01:20 +0300

    create file

---
+++ public/redux/login/loginReducer.js
@@ -0,0 +1,44 @@
+import * as actions from './loginActions';
+
+export const initialState = {
+    isLoading: false,
+    isError: false,
+    shouldRedirect: false,
+    errorMessage: '',
+    user: localStorage.getItem('auth'),
+};
+
+export default (state = initialState, action) => {
+    const { type, payload } = action;
+
+    switch(type){
+        case actions.LOGIN_REQUEST:
+            return {
+                ...state,
+                isLoading: true,
+                isError: false
+            };
+        case actions.LOGIN_SUCCESS:
+            return {
+                ...state,
+                user: payload,
+                isLoading: false,
+                isLoggedIn: true,
+                auth: true,
+            };
+        case actions.LOGIN_ERROR:
+            return {
+                ...state,
+                errorMessage: payload,
+                isLoading: false,
+                isError: true,
+            };
+        case actions.LOGOUT:
+            return {
+                ...state,
+                user: null
+            };
+        default:
+            return state;
+    }
+}
